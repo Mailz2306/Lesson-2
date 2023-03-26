2948312e5f630188bf98fcf94d122d00ff29caf6
mailz <sergey4istyak@yandex.ru>
2023-03-26 12:23:10 +0400

    Решили задачу 1

--- Program.cs
+++ Program.cs
@@ -71,7 +71,7 @@ else{
 Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 ● 34, 5 -> не кратно, остаток 4
 ● 16, 4 -> кратно
-
+123
 */ 
 
 Random random = new Random();
@@ -81,7 +81,9 @@ int firstTwoDigits = randomValue / 100;
 
 int secondDigit = randomValue % 10;
 
-Console.WriteLine($"{randomValue} -> {firstTwoDigits}{secondDigit}");
+Console.WriteLine($"{randomValue} -> {firstTwoDigits}{secondDigit}"); 
+
+
 
 
 
