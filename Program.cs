060ff5b6aca7a0ac208f672710fcc9aae85509b9
mailz <sergey4istyak@yandex.ru>
2023-03-26 15:39:14 +0400

    Решили 2 задачи

--- Program.cs
+++ Program.cs
@@ -119,17 +119,18 @@ else
 ● 5, 25 -> да ● -4, 16 -> да ● 25, 5 -> да ● 8,9 -> нет 
 
 ----------------------------
+ЗАДАЧА 14
 
-Console.WriteLine("Введите ваше число и я проверю кратно и оно 7 и 23");
+Console.WriteLine("Введите ваше число и я проверю кратно ли оно 7 и 23");
 int a = int.Parse(Console.ReadLine());
 if (a % 7 == 0 & a % 23 == 0 ){
-    Console.WriteLine($" {a} -> да");
+    Console.WriteLine($"{a} -> да");
 }
 else {
-    Console.WriteLine($" {a} -> нет");
+    Console.WriteLine($"{a} -> нет");
 }
-------------------
-*/ 
+ -----------
+ ЗАДАЧА 16 
 
 Console.WriteLine("Введите два числа и я проверю являются ли одно из них квадратом второго");
 int a = int.Parse(Console.ReadLine());
@@ -139,5 +140,66 @@ if (b == a * a | a == b * b){
 }
 else {
     Console.WriteLine($"{a},{b} -> нет, не является");
+}
+
+КОНЕЦ СЕМИНАРА 
+
+=====================================
+
+ДОМАШНЕЕ ЗАДАНИЕ 26.03.2023 
+
+ЗАДАЧА Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+РЕШЕНИЕ 
+Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+ 
+
+-------------------
+
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+
+РЕШЕНИЕ ЗАДАЧИ:
+
+int number = ReadInt("Введите трехзначное число и я покажу третью цифру в нём: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("-> Третьей цифры нет");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
 }
 
